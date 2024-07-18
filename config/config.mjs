90258e2e9b5e15287effc96e3ad3c9740db8ab45
jafari-mi <mi.jafari70@gmail.com>
2024-07-18 13:02:50 +0330

    remove old version

--- config/config.mjs
+++
@@ -1,3 +0,0 @@
-export const API_KEY = 'your_api_key';
-export const API_SECRET = 'your_api_secret';
-export const LIVEKIT_HOST = 'meet.im3.live';
--- utils/hashRoomName.mjs
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto';
-
-const hashRoomName = (roomName) => {
-  return crypto.createHash('sha256').update(roomName).digest('hex');
-};
-
-export default hashRoomName;
